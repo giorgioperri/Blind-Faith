78a6d409d4ec030b4df41ba4f91d528b3b7acb11
Rafal2707 <r.pych27@gmail.com>
2022-10-31 21:20:00 +0100

    Small typo
    Typo correction

--- Assets/Scripts/LightBeamReflection/ShootLight.cs
+++ Assets/Scripts/LightBeamReflection/ShootLight.cs
@@ -5,12 +5,12 @@ using UnityEngine;
 public class ShootLight : MonoBehaviour
 {
     public Material material;
-    LightBeam beam;
+    private LightBeam _beam;
 
     // Update is called once per frame
     void Update()
     {
         Destroy(GameObject.Find("Light Beam"));
-        beam = new LightBeam(gameObject.transform.position, gameObject.transform.right, material);
+        _beam = new LightBeam(gameObject.transform.position, gameObject.transform.right, material);
     }
 }
