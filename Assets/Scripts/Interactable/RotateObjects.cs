9c5d11b35d994c981ba72eae669c04b19b0cd209
Giorgio Perri <giorgio.perri098@gmail.com>
2022-11-15 19:48:53 +0100

    Smooth step rotation

--- Assets/Scripts/Interactable/RotateObjects.cs
+++ Assets/Scripts/Interactable/RotateObjects.cs
@@ -1,3 +1,4 @@
+using System;
 using StarterAssets;
 using System.Collections;
 using System.Collections.Generic;
@@ -6,19 +7,46 @@ using UnityEngine.InputSystem;
 
 public class RotateObjects : Interactable
 {
-
     [SerializeField] private float _angleOfRotation = 30;
+    
+    private Vector3 _desiredAngle = Vector3.zero;
+    private float _turnTime = 1f;
+    private float _rate = 60;
+
+    private bool _isTurning;
+   
     public override void OnInteraction()
     {
         if (Keyboard.current.fKey.isPressed) return;
-
-        else if (Keyboard.current.eKey.wasPressedThisFrame) transform.Rotate(0f, _angleOfRotation, 0f, Space.Self);
+        
+        if (Keyboard.current.eKey.wasPressedThisFrame && !_isTurning)
+        {
+            _isTurning = true;
+            _desiredAngle = transform.rotation.eulerAngles + new Vector3(0f, _angleOfRotation, 0f);
+            StartCoroutine(TurnTo());
+        }
     }
     public override void OnAltInteraction()
     {
         if (Keyboard.current.fKey.isPressed) return;
-
-        else if (Keyboard.current.qKey.wasPressedThisFrame) transform.Rotate(0f, -_angleOfRotation, 0f, Space.Self);
         
+        if (Keyboard.current.qKey.wasPressedThisFrame && !_isTurning)
+        {
+            _isTurning = true;
+            _desiredAngle = transform.rotation.eulerAngles - new Vector3(0f, _angleOfRotation, 0f);
+            StartCoroutine(TurnTo());
+        }
     }
+    
+    IEnumerator TurnTo () 
+    {
+        Vector3 original_angle = transform.rotation.eulerAngles;
+        for (float i = 0f; i < 1f + _turnTime / _rate; i += _turnTime / _rate) 
+        {
+            transform.rotation = Quaternion.Euler(Vector3.Lerp(original_angle, _desiredAngle, i));
+            yield return new WaitForSeconds(_turnTime/_rate);
+        }
+        _isTurning = false;
+    }
+ 
 }
