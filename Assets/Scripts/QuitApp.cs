bc23bf51cb2d1532f16ac106037a1c1bd0cafe21
Giorgio Perri <giorgio.perri098@gmail.com>
2023-01-05 12:21:18 +0100

    Icon and last thingies

--- Assets/Scripts/QuitApp.cs
+++ Assets/Scripts/QuitApp.cs
@@ -1,9 +1,15 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class QuitApp : MonoBehaviour
 {
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.None;
+    }
+
     public void QuitApplication()
     {
         Debug.Log("Quit!");
